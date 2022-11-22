5e1abf6eb3f2e2b3086f6fbc8369de2bb726366a
DUOLabs333 <dvdugo333@gmail.com>
2022-11-22 13:25:11 -0500

    Convert to CLI tool

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # Thea
-Self-hosted web-based thesaurus written in Python 3. Fork of [Synonym-web](https://github.com/andyforceno/synonym-web). I didn't want to rely on an external API, so here we are. I also removed a lot of things to make the interface more minimal.
-
-With Thea you can: query for words in your browser and get synonyms. It also offers spelling suggestions for mispelled words
+CLI tool for accessing a local thesaurus. Type word for searching. Can reference a word in a previous search with the number, csn get the current word with 0, and can get the previous word with -1. 
 
 
-## Installation:
+## Running:
     git clone https://github.com/DUOLabs333/thea.git
     ./thea/thea
-    Browse to http://localhost:5000 (can be changed with environment variable PORT)
 
 ## Attribution:
 Synonyms are from Gutenburg: 
--- src/thea/__init__.py
+++ src/thea/__init__.py
@@ -1,1 +1,45 @@
- 
+import readline,json,os
+from spellchecker import SpellChecker
+
+thesaur=json.load(open(os.path.join(os.path.dirname(__file__),'test.json'),'r'))
+
+results=[]
+prev_word=""
+curr_word=""
+def get_synonyms(word):
+    return thesaur[word] if word in thesaur else []
+
+def checkspelling(word):
+    spell = SpellChecker()
+    misspelled = spell.unknown([word])
+    for w in misspelled:
+        return [_ for _ in list(spell.candidates(w)) if _.lower() in thesaur]
+        
+while True:
+    word=input("Enter word to search: ")
+    word=word.strip().lower()
+    if word=="0":
+        word=curr_word
+    elif word=="-1":
+        word=prev_word
+    if word.isnumeric():
+        if not results:
+            print(f"Previous word {prev_word} did not have any results")
+        elif int(word)<-1 or int(word)>len(results):
+            print("Can't do that")
+            continue
+        word=results[int(word)-1]
+    prev_word=curr_word
+    curr_word=word
+    print("Word: "+word)
+    
+    results=checkspelling(word)
+    if results:
+        print(f"{word} not found. Did you mean:")
+    else:
+        results=get_synonyms(word)
+        if not results:
+            print("No synonyms found")
+            results=[]
+            
+    print(", ".join([f"{i+1}. {e}" for i,e in enumerate(results)]))
